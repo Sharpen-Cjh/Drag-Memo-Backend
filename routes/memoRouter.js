40082905b71eedd1d6482a6288e83679b48ec1c2
Sharpen-Cjh <101804186+Sharpen-Cjh@users.noreply.github.com>
2022-11-19 01:42:16 +0900

    Feat 메모 데이터 삭제 API

--- routes/memoRouter.js
+++ routes/memoRouter.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const memoController = require("./controllers/memo.controller");
 
-const { getMemo, createMemo, patchMemo } = memoController;
+const { getMemo, createMemo, patchMemo, deleteMemo } = memoController;
 
-router.post(createMemo);
+router.post("/", createMemo);
 
-router.route("/:memoId").get(getMemo).patch(patchMemo);
+router.route("/:memoId").get(getMemo).patch(patchMemo).delete(deleteMemo);
 
 module.exports = router;
